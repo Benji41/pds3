f695127e61a74a4d6f34a406521813d2433c870b
Benji41 <noereynoso.business@gmail.com>
2022-05-07 13:39:27 -0700

    Primer commit de la nueva version

---
+++ app/src/main/java/com/equipoDinamita/covidAmigo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.equipoDinamita.covidAmigo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
