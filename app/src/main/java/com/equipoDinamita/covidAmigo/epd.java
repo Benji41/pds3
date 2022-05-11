d59dfe2540c7bf7eef969dc903a8cf19b8ee5115
Irak Darek Ortiz Soqui <37721867+pirer48@users.noreply.github.com>
2022-05-11 03:11:18 -0700

    Genera el QR, falta el apartado de lectura de los QR, adición del list view a la ventana de eventos.

--- app/src/main/java/com/equipoDinamita/covidAmigo/epd.java
+++ app/src/main/java/com/equipoDinamita/covidAmigo/epd.java
@@ -5,16 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
-public class epd extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class epd extends AppCompatActivity{
 
     Button button;
+    ListView listview;
+    ArrayList<String> eventos;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_epd);
 
         button = findViewById(R.id.regresar);
+        listview = findViewById(R.id.lv);
+
+
+        eventos = new ArrayList<String>();
+        eventos.add("EVENTO1");
+        eventos.add("EVENTO2");
+        eventos.add("EVENTO3");
+
+
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, eventos);
+        listview.setAdapter(adapter);
+
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,4 +56,5 @@ public class epd extends AppCompatActivity {
         startActivity(i);
 
     }
+
 }
